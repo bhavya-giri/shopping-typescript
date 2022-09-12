61423c463c825630eeac9d41465b90a9e3ddb99a
Bhavya Giri <bhavya.giri44@gmail.com>
2022-09-12 12:35:35 +0530

    navbar links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
+import React from "react";
+import { Container, Nav, Navbar as NavbarBS } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  return <NavbarBS className="bg-white shadow-sm mb-3 ">
+        <Container>
+            <Nav>
+                <Nav.Link to='/' as ={NavLink}>Home</Nav.Link>
+                <Nav.Link to='/store' as ={NavLink}>Store</Nav.Link>
+                <Nav.Link to='/about' as ={NavLink}>About</Nav.Link>
+            </Nav>
+        </Container>
+  </NavbarBS>;
+};
 
-export default Navbar
+export default Navbar;
