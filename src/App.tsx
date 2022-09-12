91e45c8fa612323c4e5b36ed3cb18a7374f6ffa2
Bhavya Giri <bhavya.giri44@gmail.com>
2022-09-12 11:54:30 +0530

    deleted files

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
 
 function App() {
 
 
   return (
     <div className="App">
-      
+      hi
     </div>
   )
 }
